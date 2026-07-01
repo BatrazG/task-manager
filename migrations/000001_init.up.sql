31eb50ec96ab5fdceea992b13e17258fcdd291a4
BatrazG <b.gazzaev@gmail.com>
2026-07-01 17:04:39 +0300

    migration: update initial schema to include users and task relations

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,6 +1,14 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS tasks (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
+    user_id INT NOT NULL,
+    title VARCHAR(100) NOT NULL, 
     done BOOLEAN NOT NULL DEFAULT false,
-    priority VARCHAR(10) NOT NULL CHECK (priority IN ('low', 'medium', 'high'))
+    priority VARCHAR(10) NOT NULL DEFAULT 'medium' CHECK (priority IN ('low', 'medium', 'high')),
+    CONSTRAINT fk_task_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
